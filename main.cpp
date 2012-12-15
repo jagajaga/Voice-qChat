b3a8de162aeba2a21221a95c35483f7675f8a525
jagajaga <senia00@gmail.com>
2012-12-15 14:26:50 +0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+ * File:   main.cpp
+ * Author: jaga
+ *
+ * Created on December 15, 2012, 2:23 PM
+ */
+
+#include <QtGui/QApplication>
+
+int
+main (int argc, char *argv[])
+{
+  // initialize resources, if needed
+  // Q_INIT_RESOURCE(resfile);
+
+  QApplication app (argc, argv);
+
+  // create and show your widgets here
+
+  return app.exec ();
+}
